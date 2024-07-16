a14e44cd8a84b45c507407812fc1108bbeaaadf0
SarriaXD <sarria.qi.wang@gmail.com>
2024-07-16 12:44:39 -0500

    Valid Parentheses

---
+++ Valid Parentheses/Soluton.py
@@ -0,0 +1,48 @@
+# Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+#
+# An input string is valid if:
+#
+# Open brackets must be closed by the same type of brackets.
+# Open brackets must be closed in the correct order.
+# Every close bracket has a corresponding open bracket of the same type.
+#
+#
+# Example 1:
+#
+# Input: s = "()"
+# Output: true
+# Example 2:
+#
+# Input: s = "()[]{}"
+# Output: true
+# Example 3:
+#
+# Input: s = "(]"
+# Output: false
+#
+#
+# Constraints:
+#
+# 1 <= s.length <= 104
+# s consists of parentheses only '()[]{}'.
+# link https://leetcode.com/problems/valid-parentheses/description/
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        brackets = {'(': ')', '{': '}', '[': ']'}
+        stack = []
+        if len(s) % 2 == 1:
+            return False
+        for c in s:
+            if c in brackets:
+                stack.append(c)
+            else:
+                if not stack:
+                    return False
+                left_bracket = stack.pop()
+                if brackets[left_bracket] != c:
+                    return False
+        if stack:
+            return False
+        return True
+
